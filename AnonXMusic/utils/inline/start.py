a85ec6f6fbcb5033530448686abb35a2df5f464e
NotYourAstro <146406718+NotYourAstro@users.noreply.github.com>
2023-10-18 16:12:21 +0530

    Update start.py
    Signed-off-by: NotYourAstro <146406718+NotYourAstro@users.noreply.github.com>

--- AnonXMusic/utils/inline/start.py
+++ AnonXMusic/utils/inline/start.py
@@ -4,18 +4,6 @@ import config
 from AnonXMusic import app
 
 
-def start_panel(_):
-    buttons = [
-        [
-            InlineKeyboardButton(
-                text=_["S_B_1"], url=f"https://t.me/{app.username}?startgroup=true"
-            ),
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
-        ],
-    ]
-    return buttons
-
-
 def private_panel(_):
     buttons = [
         [
